90d7203d632eff05fae014d97d7faa8844a249fa
TJ Holowaychuk <tj@vision-media.ca>
2014-02-26 19:13:04 -0800

    update http-status example to use ascii-histogram

--- examples/http-status.js
+++ examples/http-status.js
@@ -1,4 +1,6 @@
 
+var histogram = require('ascii-histogram');
+
 var m = {};
 
 exports['request:end'] = function(trace){
@@ -7,55 +9,7 @@ exports['request:end'] = function(trace){
 };
 
 setInterval(function(){
-  histogram(m);
+  console.log();
+  console.log(histogram(m, { bar: '=', width: 30 }));
   m = {};
-}, 5000);
-
-/**
- * Output histogram.
- */
-
-function histogram(data) {
-  var width = 60;
-
-  data = Object.keys(data).map(function(key){
-    return {
-      key: key,
-      val: data[key]
-    }
-  });
-
-  data = data.sort(function(a, b){
-    return b.val - a.val;
-  });
-
-  var m = max(data);
-
-  console.log();
-
-  for (var i = 0; i < data.length; i++) {
-    var d = data[i];
-    var p = d.val / m;
-    var shown = Math.round(width * p);
-    var blank = width - shown
-    var bar = Array(shown).join('#');
-    bar += Array(blank).join(' ');
-    console.log('  %s | %s | %s', d.key, bar, d.val);
-  }
-
-  console.log();
-}
-
-/**
- * Max in `data`.
- */
-
-function max(data) {
-  var n = data[0].val;
-
-  for (var i = 1; i < data.length; i++) {
-    n = data[i].val > n ? data[i].val : n;
-  }
-
-  return n;
-}
+}, 1000);
