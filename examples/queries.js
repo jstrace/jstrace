0134d191e952df0183681e8f1251fc80725e20a4
TJ Holowaychuk <tj@vision-media.ca>
2014-02-28 13:21:47 -0800

    Release 0.0.5

---
+++ examples/queries.js
@@ -0,0 +1,35 @@
+
+/**
+ * Module dependencies.
+ */
+
+var http = require('http');
+var trace = require('..');
+
+var ids = 0;
+
+var server = http.createServer(function(req, res){
+  var id = ++ids;
+
+  trace('query:1:start', { id: id });
+  query1(function(){
+    trace('query:1:end', { id: id });
+
+    trace('query:2:start', { id: id });
+    query2(function(){
+      trace('query:2:end', { id: id });
+
+      res.end('hello world');
+    });
+  });
+});
+
+server.listen(3000);
+
+function query1(fn) {
+  setTimeout(fn, Math.random() * 50 | 0);
+}
+
+function query2(fn) {
+  setTimeout(fn, Math.random() * 200 | 0);
+}
